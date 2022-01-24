5ce7df6be8fb5f8843eaf4678e99514727a4f015
VinceK15 <vince_k372@yahoo.com>
2022-01-24 06:39:42 -0500

    added close and add button for form

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -23,16 +23,19 @@ setReminder(false)
     <label>Task</label>
     <input type="text" placeholder='Add Task' value={text} onChange={(e) => setText(e.target.value)}/>
 </div>
+
 <div className='form-control'>
     <label>Day & Time</label>
     <input type="text" placeholder='Add Day & Time' value={day} onChange={(e) => setDay(e.target.value)}/>
 </div>
+
 <div className='form-control form-control-check'>
     <label>Set Reminder</label>
     <input type="checkbox" checked={reminder} value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)}/>
 
     <input type="submit" value='Save Task' className='btn btn-block'/>
 </div>
+
   </form>;
 };
 
